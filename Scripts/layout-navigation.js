2dbc336fe51e5f04ea7d2d90ef3dd048fb4da220
Emre Kurtuluş <emrekurtulus76@gmail.com>
2024-12-22 00:12:52 +0300

    Add project files.

---
+++ Scripts/layout-navigation.js
@@ -0,0 +1,20 @@
+﻿function setActiveNavLink() {
+    const currentPage = window.location.pathname.split('/').pop();
+    const navLinks = document.querySelectorAll('.nav-links a');
+
+    navLinks.forEach(link => {
+        if (link.getAttribute('href').endsWith(currentPage)) {
+            link.classList.add('active');
+        }
+    });
+}
+
+// Function to navigate to the home page when the logo is clicked
+function goToHome() {
+    window.location.href = '/Home';
+}
+
+// Initialize scripts on DOMContentLoaded
+document.addEventListener('DOMContentLoaded', () => {
+    setActiveNavLink();
+});
